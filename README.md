7df1d18ea60eabf89bb409661bda6d910b216c3b
oscardavid10 <david_010@live.com.mx>
2022-04-26 22:40:10 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# peticiones_http_3_6
+ Android peticiones HTTP
---
+++ android/app/src/main/kotlin/com/example/peticiones_http_3_6/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.peticiones_http_3_6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
